69ffb011413d39414c6b06fc337e2ebbac245ede
Tim <iam@cjtim.com>
2020-09-26 22:10:16 +0700

    add body parse (allow to read req.body)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import express from 'express'
 import router from './router'
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 app.listen(process.env.PORT, () => {
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from 'express'
-
-const router = Router({ strict: true })
+const router = Router({ strict: true, caseSensitive: true })
 
 router.get('/', hi)
+router.post('/', hi)
 
 function hi(req: Request, res: Response) {
-    res.send('Hi!')
+    if (JSON.stringify(req.body) === '{}') res.send('No req.body :(')
+    res.send(req.body)
 }
+
 export default router
