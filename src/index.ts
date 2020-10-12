2d6850d22d454443d6ebc6de78ecf41b720c98e6
Tim <iam@cjtim.com>
2020-10-12 13:17:20 +0700

    add experss-session

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv'
 dotenv.config({ path: './.env' })
 import express from 'express'
+import session from 'express-session'
 import router from './router'
 import cors from 'cors'
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 const corsOption = {
     origin: ['https://localhost:3000', 'https://restaurant-helper-liff.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -14,6 +16,7 @@ const corsOption = {
     optionsSuccessStatus: 204,
 }
 app.use(cors(corsOption))
+app.use(session({ secret: 'backend'}))
 app.use(router)
 
 app.listen(process.env.PORT, () => {
