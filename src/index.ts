ecfc65a672f7d84955e05cb9a82d30bf6704832d
Tim <iam@cjtim.com>
2020-11-09 21:38:49 +0700

    cors allow *

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const corsOption = {
-    origin: ['https://localhost:3000', CONST.FRONT_END_URL],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
